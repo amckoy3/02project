89a14cba5ea91eb62753c916a68ffc36cf4252d0
Ashley Mckoy <you@example.com>
2017-02-05 20:04:38 -0500

    seperated head and tail

--- project2.pl
+++ project2.pl
@@ -2,7 +2,7 @@
  sum-up-numbers([],0).
  sum-up-numbers(L, N):-
     [L1|L2] = L,
-    sumAssist([L1|L2], N),
+    sumAssist(L2, L1, L1),
     sumHelper(L2, L1, N).
  sumHelper([L1|L2], Acc, N):-
     Sum is L1 + Acc,
