7ff89599ca8a138c8d4d1faceac690362df14bf3
Ashley Mckoy <you@example.com>
2017-02-05 23:00:50 -0500

    added a base case

--- project2.pl
+++ project2.pl
@@ -2,8 +2,11 @@
  sum-up-numbers([],0).
  sum-up-numbers(L, N):-
     [L1|L2] = L,
-    sumHelper(L2, L1, L1).
- sumHelper([L1|L2], Acc, N):-
+    sumHelper(L2, L1, N).
+    
+    sumHelper([], Acc, Acc).
+
+    sumHelper([L1|L2], Acc, N):-
     Sum is L1 + Acc,
     sumHelper(L2, Sum, N).
 
