222347da3179e84cca2c88a5623698bd1b5d6b16
Ashley Mckoy <you@example.com>
2017-02-07 01:06:05 -0500

    Added number 2 foundation

--- project2.pl
+++ project2.pl
@@ -15,3 +15,22 @@
     \+number(L1),
     sumHelper(L2,N).
 
+%2
+ sum-up-numbers-general([],0).
+ sum-up-numbers-general(L, N):-
+    [L1|L2] = L,
+    sumHelper([L1|L2], N).
+
+    sumHelper([], 0).
+
+    sumHelper([L1|L2], N):-
+    number(L1),
+    %N is L1 + Sum,
+    sumHelper(L2, Sum),
+    N is L1 + Sum.
+
+    sumHelper([L1|L2],N):-
+    \+number(L1),
+    sumHelper(L2, N).
+
+
