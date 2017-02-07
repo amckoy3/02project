c315b2a371db299e06816b443756448f10aaee6c
Ashley Mckoy <you@example.com>
2017-02-07 00:45:05 -0500

    Fixed number function

--- project2.pl
+++ project2.pl
@@ -2,15 +2,16 @@
  sum-up-numbers([],0).
  sum-up-numbers(L, N):-
     [L1|L2] = L,
-    sumHelper(L2, L1, N).
+    sumHelper([L2|L1], N).
     
-    sumHelper([], Acc, Acc).
+    sumHelper([], 0).
 
-    sumHelper([L1|L2], Acc, N):-
+    sumHelper([L1|L2], N):-
     number(L1),
-    Sum is L1 + Acc,
-    sumHelper(L2, Sum, N).
+    sumHelper(L2, Sum),
+    N is L1 + Sum.
 
-    sumHelper(L2,Sum,N):-
-    sum-up-numbers(L2,N).
+    sumHelper([L1|L2],N):-
+    \+number(L1),
+    sumHelper(L2,N).
 
